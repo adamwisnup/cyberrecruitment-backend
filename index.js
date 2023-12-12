90662fd0ffa3488544921284d431a875ad7acf5b
adamwisnup <wisnuadam21@gmail.com>
2023-12-13 02:14:52 +0700

    edit vercel.json & change index.js to main

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const participantController = require("./src/participant/participant.controller");
+
+app.use("/api/participant", participantController);
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
